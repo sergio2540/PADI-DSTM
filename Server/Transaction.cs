a33d9ec093d579db59fef5cdbf91159a17d2d398
Bruno Braga <bruno.mh.braga@gmail.com>
2014-03-26 22:16:18 +0000

    All

---
+++ Server/Transaction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Transaction
+    {
+        private long transactionId;
+        private String coordinatorAddress;
+        
+        public Transaction(long transactionId, String coordinatorAddress) {
+            this.transactionId = transactionId;
+            this.coordinatorAddress = coordinatorAddress;
+        }
+
+
+    }
+}
