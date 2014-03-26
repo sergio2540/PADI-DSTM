5615ba1135168f86f35938ad72d66bcff401d294
Bruno Braga <bruno.mh.braga@gmail.com>
2014-03-26 16:57:34 +0000

    Coordinator pseudo-implementado.

---
+++ Server/ServerApp.cs
@@ -0,0 +1,30 @@
+﻿using CommonTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting.Channels.Tcp;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class ServerApp
+    {
+
+
+
+        public static void Main(String[] args) { 
+
+            TcpChannel channel = new TcpChannel(8086); 
+            ChannelServices.RegisterChannel(channel,true);
+            ServerImpl receiver = new ServerImpl();
+            RemotingServices.Marshal((MarshalByRefObject) receiver, "Server", typeof(ServerImpl)); 
+
+            RemotingConfiguration.RegisterWellKnownServiceType(typeof(ServerImpl),"Server", WellKnownObjectMode.SingleCall);
+        
+        }
+
+    }
+}
