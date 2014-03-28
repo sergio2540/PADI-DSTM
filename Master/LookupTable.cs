9a614a953e799ea529730b5a902d32071d1ce52f
Sérgio Alves <sergio2540@gmail.com>
2014-03-28 18:54:02 +0000

    fix master

---
+++ Master/LookupTable.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master
+{
+    class LookupTable
+    {
+
+        private List<TableRow> lookupTable;
+
+        public LookupTable() 
+        {
+            lookupTable = new List<TableRow>();
+            
+        }
+
+        public List<TableRow> GetTableRows()
+        {
+            return lookupTable;
+        }
+
+        public void AddRow(TableRow row, int index) 
+        {
+
+            lookupTable.Add(row);
+
+        }
+
+        public void RemoveRow(TableRow row)
+        {
+            lookupTable.Remove(row);
+        }
+
+    }
+}
---
+++ Master/ServerPair.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Master
+{
+    class ServerPair
+    {
+
+        private string URLPrimary;
+        private string URLReplica;
+
+        public ServerPair(string primary, string replica)
+        {
+            this.URLPrimary = primary;
+            this.URLReplica = replica;
+        }
+
+        public string GetPrimary() {
+            return URLPrimary;
+        }
+
+        public string GetSecondary() {
+            return URLReplica;
+        }
+
+        public void PrintServerPair() {
+            Console.WriteLine("[ Primary: " + URLPrimary + " - " + "Secondary: " + URLReplica + "]");
+        }
+
+    }
+}
