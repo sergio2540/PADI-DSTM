c19e9358f54ecaf7ad17da3ea6ba3bc1257ea02b
Sérgio Alves <sergio2540@gmail.com>
2014-03-30 13:03:19 +0100

    add lookup service

--- DSTMServices/Services/CoordinatorService.cs
+++ DSTMServices/Services/CoordinatorService.cs
@@ -12,191 +12,175 @@ using Services;
 
 namespace DSTMServices
 {
+
+
+    class Participant
+    {
+
+        private ulong tid;
+
+        private String endpoint;
+        private IServer server;
+
+        private List<int> uids;
+
+        Participant(ulong tid, String endpoint)
+        {
+            this.tid = tid;
+            this.endpoint = endpoint;
+            //url = ....procura no master
+            //adicionar ao dicionario de strings e de referencias.
+            TcpChannel channel = new TcpChannel();
+            ChannelServices.RegisterChannel(channel, true);
+            IServer serverRef = (IServer)Activator.GetObject(typeof(IServer), endpoint);
+            this.server = serverRef;
+        }
+
+
+
+    }
+
+
+
     public class CoordinatorService
     {
-        private ulong currentTransactionId = 0;
-        
-        private DataService dataService;
+
+        private ulong currentTid = 0;
+
+        //private Dictionary<int,Participant> participantes;
+
+        private LookupService lookupService;
+
         //saber quem tem int
-        private Dictionary<int, String> uidServerAssociation;//vao haver colisões
-        private Dictionary<int, IServer> uidServerRefAssociation;
+        //private Dictionary<int, String> ServersEndpoint;//vao haver colisões
+        //private Dictionary<int, IServer> uidServerRefAssociation;
+        //private Dictionary<int, String>  uidServerAssociation = new Dictionary<int, String>();
 
-        /*
-         
-            class Participant{
-         *  
-         *  String endpoint;
-         *  IServer endpoint;
-         *  Integer[] uids;
-         * 
-         * 
-         * }
-         
-         
-         
-         */
+        public CoordinatorService()
+        {
 
+            //uidServerAssociation = new Dictionary<int, String>();
+            //uidServerRefAssociation = new Dictionary<int, IServer>();
 
-        public CoordinatorService() {
-
-            uidServerAssociation = new Dictionary<int, String>();
-            uidServerRefAssociation = new Dictionary<int, IServer>();
         }
 
         //Transaccoes
-        public bool Begin(ulong transactionId) {
+        public bool Begin(ulong transactionId)
+        {
             //uidServerAssociation = new Dictionary<int, String>();//-->idealmente isto deveria ser aqui, mas por agora fica no constructor para termos estado.
             //uidServerRefAssociation = new Dictionary<int, IServer>();
-            uidServerAssociation[1] = "tcp://localhost:8086/Server";
-            uidServerAssociation[2] = "tcp://localhost:8086/Server";
-            currentTransactionId = transactionId;
-            dataService = new DataService();
+            //uidServerAssociation[1] = "tcp://localhost:8086/Server";
+            //uidServerAssociation[2] = "tcp://localhost:8086/Server";
+            lookupService = new LookupService();
+            currentTid = transactionId;
+
+
             return true;
         }
 
         public bool Commit()//metodo deve ser repensado. pessimo codigo!!!!!!!!!!!!!!!!!!!!!!!!!
         {
-            Dictionary<String, int> endpointUidAssociation = new Dictionary<String, int>();
+
+
+            //Dictionary<String,int> getParticipantesEndpoints(uid);
+
+
             bool decision = true;
 
-            foreach (KeyValuePair<int, String> entry in uidServerAssociation)
-                if(!endpointUidAssociation.ContainsKey(entry.Value))
-                    endpointUidAssociation[entry.Value] = entry.Key;
+            List<IServer> participants = lookupService.GetParticipants();
 
-            foreach (KeyValuePair<String, int> entry in endpointUidAssociation)
-                decision &= uidServerRefAssociation[entry.Value].canCommit(currentTransactionId);  //verificar se respondeu.usar metodo ping.isto funciona assumindo que respondem
+            foreach (IServer participant in participants)
+                decision &= participant.canCommit(currentTid);  //verificar se respondeu.usar metodo ping.isto funciona assumindo que respondem
 
-            if(decision == true)
-                foreach (KeyValuePair<String, int> entry in endpointUidAssociation)
-                decision &= uidServerRefAssociation[entry.Value].doCommit(currentTransactionId);
+            bool result = true;
+
+            if (decision == true)
+                foreach (IServer participant in participants)
+                    result &= participant.doCommit(currentTid);
 
             else
-                foreach (KeyValuePair<String, int> entry in endpointUidAssociation)
-                    decision &= uidServerRefAssociation[entry.Value].doAbort(currentTransactionId);
+                foreach (IServer participant in participants)
+                    result &= participant.doAbort(currentTid);
+
             return decision;
         }
 
         public bool Abort()
         {
-            Dictionary<String, int> endpointUidAssociation = new Dictionary<String, int>();
             bool result = true;
-
-            foreach (KeyValuePair<int, String> entry in uidServerAssociation)
-                if (!endpointUidAssociation.ContainsKey(entry.Value))
-                    endpointUidAssociation[entry.Value] = entry.Key;
-
-            foreach (KeyValuePair<String, int> entry in endpointUidAssociation)
-                result &= uidServerRefAssociation[entry.Value].doAbort(currentTransactionId);
+            List<IServer> participants = lookupService.GetParticipants();
+            foreach (IServer participant in participants)
+                result &= participant.doAbort(currentTid);
 
             return result;
 
         }
 
-        public IServer InteractWithServer(int uid)
-        {//nao sei se e supost chamar isto a cada interacçao. lease
-            String endpoint = uidServerAssociation[uid];
-            IServer serverRef = null;
 
-            if (!uidServerRefAssociation.ContainsKey(uid)) //verifica se temos referencia para o servidor que guarda o uid.nao temos
-            {
-                Console.WriteLine("We have no reference for the server. Looking for uid: " + uid);
-                //url = ....procura no master
-                //adicionar ao dicionario de strings e de referencias.
-                TcpChannel channel = new TcpChannel();      //convem deitar abaixo este canal no fim
-                ChannelServices.RegisterChannel(channel, true);
-                serverRef = (IServer)Activator.GetObject(typeof(IServer), endpoint);
-                
-
-        
-                //temos de criar transaccao se o coordenador ainda não existir
-
-
-                if (serverRef == null)
-                {
-                    System.Console.WriteLine("Could not locate server");
-                    return null;
-                }
-
-
-                //este foreach so deve estar activo quando ainda não existirem endpoints por defeito.
-                //Console.WriteLine("Vamos verificar se já ligámos a este server");
-                 //foreach(String server in uidServerAssociation.Values)
-                //{
-                  //  if(server.Equals(endpoint))
-                    //    return serverRef; 
-
-//                }
-
-                 Console.WriteLine("About to begin transaction!");
-                bool canBegin = serverRef.BeginTransaction(currentTransactionId,"");
-                uidServerAssociation[uid] = endpoint;
-                uidServerRefAssociation[uid] = serverRef;
-                if (canBegin)
-                    return serverRef;
-                else return null;
-            }
-            else return uidServerRefAssociation[uid];
-           
-        }
 
 
         //PadInt
-        public int ReadPadInt(int uid) { 
-            
+        public int ReadPadInt(int uid)
+        {
+
             //remoting para server
-           // String serverUrl = null;
+            // String serverUrl = null;
             //if (!intServerMapping.ContainsKey(uid))
-                //throw new NotImplementedException();
+            //throw new NotImplementedException();
             //procura pelo servidor, adiciona ao mappeamento e busca. Adicionar evento, para detectar mudanças.
             //else serverUrl = intServerMapping[uid];
 
-            IServer serverRef = InteractWithServer(uid);
-            if (serverRef == null){
+
+            IServer serverRef = lookupService.GetServer(currentTid, uid);
+
+            if (serverRef == null)
+            {
                 System.Console.WriteLine("Coordinator failed to connect to server. Null reference returned.");//pode acontecer que nao seja possivel criar transaccao
                 return -1;//era inteligente lançar uma excepção caso nao de para ligar e outra caso nao exista.
             }
-            
-            
-            int answerValue = serverRef.ReadPadInt(currentTransactionId, uid);//quantas instancias de coordenador deveriam haver?uma transaccao de cada vez.
-           
-            
+
+
+            int answerValue = serverRef.ReadPadInt(currentTid, uid);//quantas instancias de coordenador deveriam haver?uma transaccao de cada vez.
+
+
             return answerValue;
-          
+
 
             //return null;
-        
+
         }
 
 
 
         private void WritePadInt(int uid, int value)
         {
-            
-           // String serverUrl = null;
+
+            // String serverUrl = null;
             //if(!intServerMapping.ContainsKey(uid))
-              //   throw new NotImplementedException();
-                //procura pelo servidor, adiciona ao mappeamento e busca. Adicionar evento, para detectar mudanças.se falhar procura p
+            //   throw new NotImplementedException();
+            //procura pelo servidor, adiciona ao mappeamento e busca. Adicionar evento, para detectar mudanças.se falhar procura p
             //else //faz chamada.                 //remoting para server. verificar se existe valor
-                //serverUrl = intServerMapping[uid];    
-                
-                IServer serverRef = InteractWithServer(uid);
-                if (serverRef == null)
-                {
-                    System.Console.WriteLine("Coordinator failed to connect to server. Null reference returned.");
-                    return;//era inteligente lançar uma excepção caso nao de para ligar e outra caso nao exista.
-                    //o master tem de ser consultado
-                }
-                serverRef.WritePadInt(currentTransactionId,uid,value);
-             
+            //serverUrl = intServerMapping[uid];    
+
+            IServer serverRef = lookupService.GetServer(currentTid, uid);
+
+            if (serverRef == null)
+            {
+                System.Console.WriteLine("Coordinator failed to connect to server. Null reference returned.");
+                return;//era inteligente lançar uma excepção caso nao de para ligar e outra caso nao exista.
+                //o master tem de ser consultado
+            }
+            serverRef.WritePadInt(currentTid, uid, value);
+
         }
 
         public PadInt CreatePadInt(int uid)
         {
             Console.WriteLine("CreatePadint called with: " + uid);
-            IServer serverRef = InteractWithServer(uid);
+            IServer serverRef = lookupService.GetServer(currentTid, uid);
 
 
-            PadInt padInt = serverRef.CreatePadInt(currentTransactionId,uid);
+            PadInt padInt = serverRef.CreatePadInt(currentTid, uid);
 
             if (padInt == null)
                 return null;
@@ -221,12 +205,13 @@ namespace DSTMServices
             return padInt;
         }
 
-        public void OnPadintChange(Object sender ,EventArgs e) {
+        public void OnPadintChange(Object sender, EventArgs e)
+        {
             Console.WriteLine("change event  called!");
 
-            PadIntLocal changedPadInt = (PadIntLocal) sender;
+            PadIntLocal changedPadInt = (PadIntLocal)sender;
             changedPadInt.readHandler -= OnPadintRead;
-            WritePadInt(changedPadInt.Uid,changedPadInt.Read());
+            WritePadInt(changedPadInt.Uid, changedPadInt.Read());
             changedPadInt.readHandler += OnPadintRead;
 
         }
@@ -243,6 +228,6 @@ namespace DSTMServices
         }
 
 
-        
+
     }
 }
